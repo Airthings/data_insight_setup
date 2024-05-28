223b101b0e1b1c15034dc214fe74ca9e5b999b58
Sander Nordeide <nordeidesander@gmail.com>
2024-05-28 15:53:07 +0200

    add script for windows

---
+++ install_script_windows.sh
@@ -0,0 +1,18 @@
+# Set execution policy to bypass for this process
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Ensure TLS 1.2 is used for the download
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+
+# Download and execute the Chocolatey installation script
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install Git
+choco install git -y
+
+# Install AWS CLI
+choco install awscli -y
+
+# Install pyenv
+choco install pyenv-win -y
+
