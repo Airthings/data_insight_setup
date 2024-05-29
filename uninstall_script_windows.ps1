2d560b493493876c328373a4f78f91f816ef1195
Sander Nordeide <nordeidesander@gmail.com>
2024-05-29 12:59:09 +0200

    update uninstall windows script

--- uninstall_script_windows.ps1
+++ uninstall_script_windows.ps1
@@ -4,41 +4,73 @@ function Uninstall-Package {
         [string]$packageName
     )
 
-    # Check if the package is installed
-    $packageInstalled = winget list | Select-String -Pattern $packageName
-    if ($packageInstalled) {
-        Write-Host "Uninstalling $packageName..."
-        winget uninstall --id $packageName --silent --accept-package-agreements --accept-source-agreements
-    } else {
-        Write-Host "$packageName is not installed."
+    try {
+        # Check if the package is installed
+        $packageInstalled = winget list | Select-String -Pattern $packageName
+        if ($packageInstalled) {
+            Write-Host "Uninstalling $packageName..."
+            winget uninstall --id $packageName --silent --accept-package-agreements --accept-source-agreements
+        } else {
+            Write-Host "$packageName is not installed."
+        }
+    } catch {
+        Write-Host "An error occurred while uninstalling $packageName: $_"
     }
 }
 
 # Uninstall poetry using pipx
-pipx uninstall poetry
+try {
+    pipx uninstall poetry
+} catch {
+    Write-Host "An error occurred while uninstalling poetry: $_"
+}
 
 # Uninstall pipx using Scoop
-scoop uninstall pipx
+try {
+    scoop uninstall pipx
+} catch {
+    Write-Host "An error occurred while uninstalling pipx: $_"
+}
 
 # Remove Scoop from the path
-$scoopPath = "$($env:USERPROFILE)\scoop\shims"
-[System.Environment]::SetEnvironmentVariable('PATH', $env:PATH -replace ";$scoopPath", '', [System.EnvironmentVariableTarget]::Machine)
-$env:PATH = $env:PATH -replace ";$scoopPath", ''
+try {
+    $scoopPath = "$($env:USERPROFILE)\scoop\shims"
+    [System.Environment]::SetEnvironmentVariable('PATH', $env:PATH -replace ";$scoopPath", '', [System.EnvironmentVariableTarget]::Machine)
+    $env:PATH = $env:PATH -replace ";$scoopPath", ''
+} catch {
+    Write-Host "An error occurred while removing Scoop from the PATH: $_"
+}
 
 # Uninstall Scoop (deletes scoop directory)
-Remove-Item -Recurse -Force "$($env:USERPROFILE)\scoop"
+try {
+    Remove-Item -Recurse -Force "$($env:USERPROFILE)\scoop"
+} catch {
+    Write-Host "An error occurred while uninstalling Scoop: $_"
+}
 
 # Uninstall pyenv specific version of Python
-pyenv uninstall 3.9.7
+try {
+    pyenv uninstall 3.9.7
+} catch {
+    Write-Host "An error occurred while uninstalling Python 3.9.7 with pyenv: $_"
+}
 
 # Reset pyenv global to system
-pyenv global system
+try {
+    pyenv global system
+} catch {
+    Write-Host "An error occurred while resetting pyenv global: $_"
+}
 
 # Remove pyenv from environment variables
-$env:PYENV = "$($env:USERPROFILE)\.pyenv"
-$env:PATH = $env:PATH -replace "$env:PYENV\pyenv-win\bin;", ''
-$env:PATH = $env:PATH -replace "$env:PYENV\pyenv-win\shims;", ''
-[System.Environment]::SetEnvironmentVariable('PATH', $env:PATH, [System.EnvironmentVariableTarget]::Process)
+try {
+    $env:PYENV = "$($env:USERPROFILE)\.pyenv"
+    $env:PATH = $env:PATH -replace "$env:PYENV\pyenv-win\bin;", ''
+    $env:PATH = $env:PATH -replace "$env:PYENV\pyenv-win\shims;", ''
+    [System.Environment]::SetEnvironmentVariable('PATH', $env:PATH, [System.EnvironmentVariableTarget]::Process)
+} catch {
+    Write-Host "An error occurred while removing pyenv from environment variables: $_"
+}
 
 # Uninstall pyenv
 Uninstall-Package -packageName "pyenv.pyenv"
@@ -50,13 +82,21 @@ Uninstall-Package -packageName "Amazon.AWSCLI"
 Uninstall-Package -packageName "Git.Git"
 
 # Uninstall Chocolatey
-choco uninstall -y chocolatey
+try {
+    choco uninstall -y chocolatey
+} catch {
+    Write-Host "An error occurred while uninstalling Chocolatey: $_"
+}
 
 # Remove Chocolatey environment variables
-$chocoPath = "$($env:SystemDrive)\ProgramData\chocolatey\bin"
-$env:PATH = $env:PATH -replace ";$chocoPath", ''
-[System.Environment]::SetEnvironmentVariable('PATH', $env:PATH, [System.EnvironmentVariableTarget]::Process)
-[System.Environment]::SetEnvironmentVariable('PATH', $env:PATH, [System.EnvironmentVariableTarget]::Machine)
+try {
+    $chocoPath = "$($env:SystemDrive)\ProgramData\chocolatey\bin"
+    $env:PATH = $env:PATH -replace ";$chocoPath", ''
+    [System.Environment]::SetEnvironmentVariable('PATH', $env:PATH, [System.EnvironmentVariableTarget]::Process)
+    [System.Environment]::SetEnvironmentVariable('PATH', $env:PATH, [System.EnvironmentVariableTarget]::Machine)
+} catch {
+    Write-Host "An error occurred while removing Chocolatey from the PATH: $_"
+}
 
 Write-Host "All specified packages have been uninstalled and environment variables reset."
 
